f01e522df8d3894d665181c17905cad93cb11bf5
Hack Reactor Students <github@hackreactor.com>
2013-08-06 11:22:00 -0700

    each passes all tests

--- src/underbar.js
+++ src/underbar.js
@@ -37,8 +37,14 @@ var _ = { };
   // Call iterator(value, key, collection) for each element of collection.
   // Accepts both arrays and objects.
   _.each = function(collection, iterator) {
-    for(var i = 0; i < collection.length; i++) {
-      iterator(collection[i], i, collection);
+    if(Array.isArray(collection)) {
+      for(var i = 0; i < collection.length; i++) {
+        iterator(collection[i], i, collection);
+      }
+    } else {
+      for (var key in collection) {
+        iterator(collection[key], key, collection);
+      }
     }
   };
 
