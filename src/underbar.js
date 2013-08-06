303574544215722f397e588d29b9d822e5a80f0f
Hack Reactor Students <github@hackreactor.com>
2013-08-06 15:59:50 -0700

    _.some passes all tests

--- src/underbar.js
+++ src/underbar.js
@@ -185,6 +185,10 @@ var _ = { };
   // provided, provide a default one
   _.some = function(collection, iterator) {
     // TIP: There's a very clever way to re-use every() here.
+    iterator = iterator || function(val){return val};
+    return !_.every(collection, function(val){
+      return !iterator(val);
+    });
   };
 
 
