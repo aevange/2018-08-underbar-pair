9688794d9123109f33ba0bc0e3e27fbc0fedfb9e
Hack Reactor Students <github@hackreactor.com>
2013-08-06 16:21:00 -0700

    _.defaults passes all tests

--- src/underbar.js
+++ src/underbar.js
@@ -222,6 +222,12 @@ var _ = { };
   // Like extend, but doesn't ever overwrite a key that already
   // exists in obj
   _.defaults = function(obj) {
+    _.each(arguments, function(arg, index){
+      _.each(arg, function(data, key){
+        obj.hasOwnProperty(key) || (obj[key] = data);
+      });
+    });
+    return obj;
   };
 
 
